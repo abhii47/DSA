669ef0a61999f2ca047a379fd5500c083d1d5320
abhii47 <150163008+abhii47@users.noreply.github.com>
2025-07-28 21:44:18 +0530

    Time: 16 ms (91.07%) | Memory: 56.2 MB (47.36%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int low = 0, high = nums[nums.length - 1] - nums[0];
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (canFormPairs(nums, p, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private boolean canFormPairs(int[] nums, int p, int maxDiff) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) {
+                count++;
+                i++; // skip next index to prevent overlapping
+            }
+        }
+        return count >= p;
+    }
+}
